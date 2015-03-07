08c8ca909f6628088aaf3e75407c492f7cc2fec7
Matthew Grossman <matt@mrgrossman.com>
2015-03-06 21:20:45 -0500

    Added in vundle cloning

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 ln -s $HOME/dotfiles/.vim $HOME/.vim
 ln -s $HOME/dotfiles/.ycm_extra_conf.py $HOME/.ycm_extra_conf
 ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
