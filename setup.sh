9f6c8bac12bff29dcc80d691d713c6b259064173
Matthew Grossman <matthewryangrossman@gmail.com>
2015-09-09 19:27:26 -0400

    Sorry prezto, symlinking hasn't been fun

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
+# symlink the dotfiles
 ln -s $HOME/dotfiles/.vim $HOME/.vim
 ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 
+# base16 color scheme
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+
+# vim/vundle setup
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
