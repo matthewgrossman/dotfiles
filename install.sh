d85aab053be4f003c0aa5971a343cb333865890c
Matthew Grossman <matthewryangrossman@gmail.com>
2015-11-11 16:39:15 -0500

    changed tmux settings with new prefix

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+# ensure vim, tmux, and git are all installed
+
 # base16 color scheme
 git clone https://github.com/chriskempson/base16-shell.git $HOME/dotfiles/base16-shell
 
@@ -9,3 +11,6 @@ ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 # vim/vundle setup
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
+# tmux setup
+ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
