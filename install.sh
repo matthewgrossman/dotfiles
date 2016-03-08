c30a4a026bc00a36fc808201f9f4269edf1066ae
Matthew Grossman <matthewryangrossman@gmail.com>
2016-03-08 16:26:36 -0500

    Added plugins and stuff

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 # symlink the dotfiles
 ln -s $HOME/dotfiles/.vim $HOME/.vim
+ln -s $HOME/dotfiles/ideavimrc $HOME/.ideavimrc
 ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
 
