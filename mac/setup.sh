ea2d1e4579045b599c913ce41128d3bf1414e969
Matthew Grossman <mgrossman@lyft.com>
2017-06-04 17:10:20 -0700

    make setup.sh install the iterm2 settings

--- mac/setup.sh
+++ mac/setup.sh
@@ -16,6 +16,10 @@ chsh -s $(which zsh)
 # base16-shell colors
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
+# sync iterm2 with config
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$HOME/dotfiles/mac"
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
 # reload ZSH now that setup is done
 source ~/.zshrc
 zplug install
