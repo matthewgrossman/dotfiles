42ea6a61202df5e684b870ffd1771cb9786439b9
Matthew Grossman <mgrossman@lyft.com>
2017-08-29 12:25:19 -0700

    Fix up theme.zsh

--- theme.zsh
+++ theme.zsh
@@ -1,14 +1,23 @@
+# function to colorize text
+zc(){
+    # $ zc 'hello world' 'red' 'white'
+    local text=$1  # text to colorize
+    local cfg=$2  # foreground color
+    local cbg=$3  # background color
+
+    local colors="%{$bg[${cbg}]%}%{$fg[${cfg}]%}"
+    echo "${colors} ${text} ${reset_color}"
+}
+
+# return git branch, yellow bg if there are diffs, green bg if there aren't
 git_status() {
     local gstatus=$(git status --short --untracked-files=no)
     [ -n "$gstatus" ] && local status_color='yellow' || local status_color='green'
-    local color="%{$bg[${status_color}]%}%{$fg[black]%}"
     local branch=$(git rev-parse --abbrev-ref HEAD)
-    echo "${color} ${branch}"
+    zc $branch 'black' $status_color
 }
 
-
 setopt prompt_subst
-local path_name="%{$bg[blue]%}%{$fg[black]%} %~"
+local path_name=$(zc '%~' 'black' 'blue')
 local leading=$'\u00BB'
-PROMPT="${path_name} \$(git_status) %{$reset_color%} ${leading} "
-
+PROMPT="${path_name}\$(git_status) ${leading} "
