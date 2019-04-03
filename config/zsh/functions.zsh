8eabf316d8e18df47abd58c10de5bcb0973acc24
Matthew Grossman <matthewryangrossman@gmail.com>
2019-04-03 10:08:19 -0700

    fix things

--- config/zsh/functions.zsh
+++ config/zsh/functions.zsh
@@ -45,10 +45,10 @@ src() {
     fi
 }
 
+# TODO mess w/ this more pls
 _fzf_complete_git() {
     local branches
     if [ "$*" = "git co " ]; then
-        echo "inside"
         branches=$(git branch --sort=committerdate | awk '/^[^*]/ {print $1}')
         _fzf_complete "--tac --no-sort" "$*" < <(echo "$branches")
     fi
