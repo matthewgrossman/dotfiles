44b68465e9f79de2ea329c8e30d8ae6f7a6078d9
Matthew Grossman <matthewryangrossman@gmail.com>
2020-02-21 14:24:23 -0800

    more shenanigans with dotfiles

--- config/zsh/alias.sh
+++ config/zsh/alias.sh
@@ -1,8 +1,7 @@
 alias ls="ls -G"
-alias dot="cd $HOME/dotfiles"
+alias dot='cd $HOME/dotfiles'
 alias g="git"
 
 alias vi='nvim'
 alias vim='nvim'
 alias k='kubectl'
-# alias vim="$HOME/Downloads/nvim-osx64/bin/nvim"
