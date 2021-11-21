a3157dbeb1531941d85c6986c64fe5b40c72c61e
Matthew Grossman <matthewryangrossman@gmail.com>
2021-11-21 15:58:08 -0800

    even more

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -26,30 +26,53 @@ local on_attach = function(client, bufnr)
   -- buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   -- buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  -- buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  -- buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  -- buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
   -- buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-  -- buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  -- buf_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+
+  -- vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
 
 end
+-- to debug do this:
+-- vim.lsp.set_log_level("debug")
+-- :lua vim.cmd('e'..vim.lsp.get_log_path())
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { "pylsp", "tsserver", "gopls", "clangd"}
+
+local servers = {"pylsp", "tsserver", "gopls"}
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
     flags = {
       debounce_text_changes = 150,
+    },
+    settings = {
+      pylsp = {
+        configurationSources = { "flake8"}
+      }
     }
   }
 end
 
--- temporarily disable publishDiagnostics until I've migrated over ALE
-vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
+nvim_lsp.efm.setup {
+    init_options = {documentFormatting = true},
+    on_attach = on_attach,
+    filetypes = { "lua" }
+    settings = {
+        rootMarkers = {".git/"},
+        languages = {
+            lua = {
+                {formatCommand = "lua-format -i", formatStdin = true}
+            }
+        }
+    }
+}
 
-
+-- -- temporarily disable publishDiagnostics until I've migrated over ALE
+-- vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
 
 -- compe
 vim.o.completeopt = "menuone,noselect"
