e3bae49c03fd46bdeb9e0878708fecac9a513489
Matthew Grossman <matthewryangrossman@gmail.com>
2021-12-05 11:36:36 -0800

    fix symlink

--- link.sh
+++ link.sh
@@ -1,4 +1,4 @@
 # symlink the dotfiles
-ln -s $HOME/dotfiles/config $HOME/.config
+ln -s $HOME/dotfiles/config/ $HOME/.config
 ln -s $HOME/dotfiles/config/zsh/zshrc $HOME/.zshrc
 ln -s $HOME/dotfiles/ctags $HOME/.ctags.d
