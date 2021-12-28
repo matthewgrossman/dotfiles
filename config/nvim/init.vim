8ab5a5662e9ef553308ac32421a03027d83ef848
Matthew Grossman <matthewryangrossman@gmail.com>
2021-12-28 02:36:40 -0800

    so many changes, but getting null ls working

--- config/nvim/init.vim
+++ config/nvim/init.vim
@@ -170,8 +170,13 @@ Plug 'jose-elias-alvarez/null-ls.nvim'
 Plug 'tpope/vim-commentary'
 Plug 'jiangmiao/auto-pairs'
 Plug 'neovim/nvim-lspconfig'
-Plug 'hrsh7th/nvim-compe'
-
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-vsnip'
+Plug 'hrsh7th/vim-vsnip'
 
 " file management
 Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --all'  }
--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -1,67 +1,141 @@
 local M = {}
-local nvim_lsp = require('lspconfig')
+local nvim_lsp = require("lspconfig")
 local map = vim.api.nvim_set_keymap
 
-require('gitsigns').setup()
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr) -- luacheck: ignore
-    local function buf_set_keymap(...)
-        vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
-    local function buf_set_option(...)
-        vim.api.nvim_buf_set_option(bufnr, ...)
-    end
+require("gitsigns").setup()
 
-    -- Enable completion triggered by <c-x><c-o>
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+-- nvim-cmp {{{
+vim.o.completeopt = "menu,menuone,noselect"
+local cmp = require("cmp")
 
-    -- Mappings.
-    local opts = {noremap = true, silent = true}
-
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    -- buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', '<C-]>', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    -- buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    -- buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    -- buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    -- buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    -- buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-    -- buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    -- buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    buf_set_keymap('n', '<leader>e',
-                   '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
-                   opts)
-    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>',
-                   opts)
-    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>',
-                   opts)
-    -- buf_set_keymap('n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-    -- buf_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-
-    buf_set_keymap('n', 'gf', '<cmd>lua vim.lsp.buf.formatting_seq_sync()<CR>',
-                   opts)
-    -- vim.api
-    --     .nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting()]]
-
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+		end,
+	},
+	mapping = {
+		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+		["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+		["<C-e>"] = cmp.mapping({
+			i = cmp.mapping.abort(),
+			c = cmp.mapping.close(),
+		}),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<Tab>"] = function(fallback)
+			if not cmp.select_next_item() then
+				if vim.bo.buftype ~= "prompt" and has_words_before() then
+					cmp.complete()
+				else
+					fallback()
+				end
+			end
+		end,
+
+		["<S-Tab>"] = function(fallback)
+			if not cmp.select_prev_item() then
+				if vim.bo.buftype ~= "prompt" and has_words_before() then
+					cmp.complete()
+				else
+					fallback()
+				end
+			end
+		end,
+	},
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "vsnip" }, -- For vsnip users.
+	}, {
+		{ name = "buffer" },
+	}),
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline("/", {
+	sources = {
+		{ name = "buffer" },
+	},
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(":", {
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
+})
+-- }}}
+
+-- vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+	-- Enable underline, use default values
+	underline = true,
+	-- Enable virtual text, override spacing to 4
+	virtual_text = {
+		spacing = 4,
+	},
+	-- Disable a feature
+	update_in_insert = false,
+})
+
+-- keybinds for formatting/diagnostics
+local on_attach_null_ls = function(client, bufnr) -- luacheck: ignore
+	local opts = { noremap = true, silent = true }
+	local function buf_set_keymap(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
+
+	buf_set_keymap("n", "gf", "<cmd>lua vim.lsp.buf.formatting_seq_sync()<CR>", opts)
+
+	buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+	buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+	buf_set_keymap("n", "<leader>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+end
+
+local on_attach = function(client, bufnr) -- luacheck: ignore
+	local function buf_set_keymap(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
+	local function buf_set_option(...)
+		vim.api.nvim_buf_set_option(bufnr, ...)
+	end
+
+	-- enable completion triggered by <c-x><c-o>
+	buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+
+	-- mappings.
+	local opts = { noremap = true, silent = true }
+
+	-- see `:help vim.lsp.*` for documentation on any of the below functions
+	-- buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+	buf_set_keymap("n", "<c-]>", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+	buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+	-- buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+	-- buf_set_keymap('n', '<c-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+	-- buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>', opts)
+	-- buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>', opts)
+	-- buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>', opts)
+	-- buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+	-- buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+	buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+	-- buf_set_keymap('n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>', opts)
+	-- buf_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<cr>", opts)
+
+	on_attach_null_ls(client, bufnr)
+	-- vim.api
+	--     .nvim_command [[autocmd bufwritepre <buffer> lua vim.lsp.buf.formatting()]]
+end
+
 -- to debug do this:
 vim.lsp.set_log_level("debug")
 -- :lua vim.cmd('e'..vim.lsp.get_log_path())
-
--- Use a loop to conveniently call 'setup' on multiple servers and
--- map buffer local keybindings when the language server attaches
-
-local servers = {"pylsp", "tsserver", "gopls", "rust_analyzer"}
-for _, lsp in ipairs(servers) do
-    nvim_lsp[lsp].setup {
-        on_attach = on_attach,
-        flags = {debounce_text_changes = 150},
-        settings = {pylsp = {configurationSources = {"flake8"}}}
-    }
-end
 
 -- TODO get clangd working w/ envoy
 -- nvim_lsp.clangd.setup {
@@ -69,121 +143,45 @@ end
 --     on_attach = on_attach
 -- }
 
--- print(vim.inspect(vim.tbl_keys(efmlinters)))
--- nvim_lsp.efm.setup {
---     init_options = {documentFormatting = true},
---     on_attach = on_attach,asdfasdf
---     filetypes = vim.tbl_keys(efmlinters),
---     settings = {rootMarkers = {".git/"}, languages = efmlinters}
--- }
 require("null-ls").setup({
-        debug = true,
-    sources = {
-        require("null-ls").builtins.formatting.stylua,
-        require("null-ls").builtins.formatting.black,
-        require("null-ls").builtins.diagnostics.luacheck.with({
-                extra_args = {"--config", vim.fn.expand("$XDG_CONFIG_HOME/luacheck/.luacheckrc")
-            }}),
-        require("null-ls").builtins.diagnostics.mypy.with({
-                extra_args = {"--ignore-missing-imports"}
-            }),
-        require("null-ls").builtins.formatting.reorder_python_imports,
-        require("null-ls").builtins.diagnostics.shellcheck,
-        require("null-ls").builtins.formatting.trim_whitespace
-    },
-    on_attach=on_attach
+	debug = true,
+	sources = {
+		require("null-ls").builtins.formatting.stylua,
+		require("null-ls").builtins.formatting.black,
+		require("null-ls").builtins.diagnostics.luacheck.with({
+			extra_args = { "--config", vim.fn.expand("$XDG_CONFIG_HOME/luacheck/.luacheckrc") },
+		}),
+		require("null-ls").builtins.diagnostics.mypy.with({
+			extra_args = { "--ignore-missing-imports" },
+		}),
+		require("null-ls").builtins.formatting.reorder_python_imports,
+		require("null-ls").builtins.diagnostics.shellcheck,
+		require("null-ls").builtins.formatting.trim_whitespace,
+	},
+	on_attach = on_attach_null_ls,
 })
-
-
--- vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics, {
-        -- Enable underline, use default values
-        underline = true,
-        -- Enable virtual text, override spacing to 4
-        virtual_text = {
-            spacing = 4,
-        },
-        -- Disable a feature
-        update_in_insert = false,
-    }
-    )
-
-
--- compe
-vim.o.completeopt = "menuone,noselect"
-require'compe'.setup {
-    enabled = true,
-    autocomplete = true,
-    debug = false,
-    min_length = 1,
-    preselect = 'enable',
-    throttle_time = 80,
-    source_timeout = 200,
-    resolve_timeout = 800,
-    incomplete_delay = 400,
-    max_abbr_width = 100,
-    max_kind_width = 100,
-    max_menu_width = 100,
-    documentation = {
-        border = {'', '', '', ' ', '', '', '', ' '}, -- the border option is the same as `|help nvim_open_win|`
-        winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
-        max_width = 120,
-        min_width = 60,
-        max_height = math.floor(vim.o.lines * 0.3),
-        min_height = 1
-    },
-
-    source = {
-        path = true,
-        buffer = true,
-        calc = true,
-        nvim_lsp = true,
-        nvim_lua = true
-    }
-}
-local t = function(str)
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
+-- Setup lspconfig.
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local servers = { "pylsp", "tsserver", "gopls", "rust_analyzer" }
+for _, lsp in ipairs(servers) do
+	nvim_lsp[lsp].setup({
+		on_attach = on_attach,
+		flags = { debounce_text_changes = 250 },
+		capabilities = capabilities,
+		settings = { pylsp = { configurationSources = { "flake8" } } },
+	})
 end
 
-local check_back_space = function()
-    local col = vim.fn.col('.') - 1
-    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
-end
-
--- Use (s-)tab to:
---- move to prev/next item in completion menuone
---- jump to prev/next snippet's placeholder
-_G.tab_complete = function()
-    if vim.fn.pumvisible() == 1 then
-        return t "<C-n>"
-    elseif check_back_space() then
-        return t "<Tab>"
-    else
-        return vim.fn['compe#complete']()
-    end
-end
-_G.s_tab_complete = function()
-    if vim.fn.pumvisible() == 1 then
-        return t "<C-p>"
-    else
-        -- If <S-Tab> is not working in your terminal, change it to <C-h>
-        return t "<S-Tab>"
-    end
-end
-
-require("telescope").setup({defaults = {preview = false}})
-require('telescope').load_extension('fzf')
+-- telescope {{{
+require("telescope").setup({ defaults = { preview = false } })
+require("telescope").load_extension("fzf")
 M.project_files = function()
-    local ok = pcall(require"telescope.builtin".git_files, {show_untracked = false})
-    if not ok then require"telescope.builtin".find_files() end
+	local ok = pcall(require("telescope.builtin").git_files, { show_untracked = false })
+	if not ok then
+		require("telescope.builtin").find_files()
+	end
 end
-map('n', '<C-p>',
-    "<Cmd>lua require('init').project_files()<CR>",
-    {noremap = true})
-
-map("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-map("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-map("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+map("n", "<C-p>", "<Cmd>lua require('init').project_files()<CR>", { noremap = true })
+-- }}}
 
 return M
