0168dad3c79dd9d1776599a4f31434846bfe40ac
Matthew Grossman <matthewryangrossman@gmail.com>
2022-04-21 23:16:51 -0700

    gitsigns

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -2,7 +2,34 @@ local M = {}
 local nvim_lsp = require("lspconfig")
 local map = vim.api.nvim_set_keymap
 
-require("gitsigns").setup()
+require('gitsigns').setup{
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk{wrap=false} end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    map('n', '[c', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk{wrap=false} end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    -- Text object
+    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+  end
+  }
+
 require("nvim-autopairs").setup({})
 require('lualine').setup {
   options = { theme  = 'auto' },
