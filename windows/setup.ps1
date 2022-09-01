a1b460f07a7dad17f04e07c88408e5e761bf907e
Matthew Grossman <matthewryangrossman@gmail.com>
2022-09-01 13:27:32 -0700

    add in choco

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,1 +1,9 @@
-winget import $PSScriptRoot\winget.json
+# winget import $PSScriptRoot\winget.json
+
+# install chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iwr https://community.chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+choco install razer-synapse-3
+choco install 7zip
+
+wsl --install --distribution Ubuntu
