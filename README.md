94eccb2168ec48dc24ea32b6e534b909529cc0c3
Matthew Grossman <matthewryangrossman@gmail.com>
2022-09-01 13:42:00 -0700

    more changes to setup.ps1

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 1. Open `Powershell` as administrator
 1. Run the following: 
 ```powershell
-git clone https://github.com/matthewgrossman/dotfiles; . .\dotfiles\windows\setup.ps1
+cd $HOME; git clone https://github.com/matthewgrossman/dotfiles; . .\dotfiles\windows\setup.ps1
 ```
 
--- windows/setup.ps1
+++ windows/setup.ps1
@@ -3,7 +3,14 @@
 # install chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iwr https://community.chocolatey.org/install.ps1 -UseBasicParsing | iex
 
-choco install razer-synapse-3
-choco install 7zip
+choco install -y razer-synapse-3
+choco install -y 7zip
+
+# install scoop {{{
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
+irm get.scoop.sh | iex
+
+scoop install win32yank
+# }}}
 
 wsl --install --distribution Ubuntu
