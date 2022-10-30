fda6fedd89a830651b14c55b32c3be59ca02a51f
Matthew Grossman <matthewryangrossman@gmail.com>
2022-10-29 20:04:47 -0700

    fix the padding/resizing for weztemr

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -4,4 +4,11 @@ return {
     font = wezterm.font("Fira Code"),
     color_scheme = "OneDark (base16)",
     enable_kitty_keyboard = true,
+    window_decorations = "RESIZE",
+    window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0,
+    },
 }
