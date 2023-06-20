5306c91786b5248652629efae3455ab575a54151
Matthew Grossman <matthewryangrossman@gmail.com>
2023-06-20 16:24:44 -0700

    more wezterm config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,26 +1,49 @@
 local wezterm = require('wezterm')
 local act = wezterm.action
 
-
 local config = {
-  font = wezterm.font("Fira Code"),
+  font = "Fira Code",
+  font_rules = {
+    {
+      italic = true,
+      font = wezterm.font {
+        family = 'JetBrains Mono',
+      },
+    },
+  },
+  font_size = 15.0,
+  default_cwd = "TODO",
   color_scheme = "OneDark (base16)",
   window_decorations = "RESIZE",
-  enable_kitty_keyboard = true,
+  window_frame = {
+    font_size = 15.0
+  },
   window_padding = {
     left = 0,
     right = 0,
     top = 0,
     bottom = 0,
   },
-  debug_key_events = true,
   -- debug_key_events = true,
   -- default_gui_startup_args = { 'connect', 'WSL:Ubuntu'},
-  -- keys = {
-  --     {key="/", mods="CTRL", action=wezterm.action{SendString="\x1f"}},
-  -- }
 }
-config.keys = {}
+config.keys = {
+  -- Rebind OPT-Left, OPT-Right as ALT-b, ALT-f respectively to match Terminal.app behavior
+  {
+    key = 'LeftArrow',
+    mods = 'OPT',
+    action = act.SendKey {
+      key = 'b',
+      mods = 'ALT',
+    },
+  },
+  {
+    key = 'RightArrow',
+    mods = 'OPT',
+    action = act.SendKey { key = 'f', mods = 'ALT' },
+  },
+}
+
 for i = 1, 8 do
   -- CTRL+ALT + number to activate that tab
   table.insert(config.keys, {
