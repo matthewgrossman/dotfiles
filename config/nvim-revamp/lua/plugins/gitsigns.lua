a8d26c95766b22e5d1b38cbbcb7e777d6c938856
Matthew Grossman <matthewryangrossman@gmail.com>
2024-08-19 23:27:51 -0700

    change stylua.toml to 3 spaces

--- config/nvim-revamp/lua/plugins/gitsigns.lua
+++ config/nvim-revamp/lua/plugins/gitsigns.lua
@@ -1,38 +1,38 @@
 return { -- Adds git related signs to the gutter, as well as utilities for managing changes
-	"lewis6991/gitsigns.nvim",
-	opts = {
-		signs = {
-			add = { text = "+" },
-			change = { text = "~" },
-			delete = { text = "_" },
-			topdelete = { text = "‾" },
-			changedelete = { text = "~" },
-		},
-		on_attach = function(bufnr)
-			local gitsigns = require("gitsigns")
+   "lewis6991/gitsigns.nvim",
+   opts = {
+      signs = {
+         add = { text = "+" },
+         change = { text = "~" },
+         delete = { text = "_" },
+         topdelete = { text = "‾" },
+         changedelete = { text = "~" },
+      },
+      on_attach = function(bufnr)
+         local gitsigns = require("gitsigns")
 
-			local function map(mode, l, r, opts)
-				opts = opts or {}
-				opts.buffer = bufnr
-				vim.keymap.set(mode, l, r, opts)
-			end
+         local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+         end
 
-			-- Navigation
-			map("n", "]c", function()
-				if vim.wo.diff then
-					vim.cmd.normal({ "]c", bang = true })
-				else
-					gitsigns.nav_hunk("next")
-				end
-			end)
+         -- Navigation
+         map("n", "]c", function()
+            if vim.wo.diff then
+               vim.cmd.normal({ "]c", bang = true })
+            else
+               gitsigns.nav_hunk("next")
+            end
+         end)
 
-			map("n", "[c", function()
-				if vim.wo.diff then
-					vim.cmd.normal({ "[c", bang = true })
-				else
-					gitsigns.nav_hunk("prev")
-				end
-			end)
-		end,
-	},
+         map("n", "[c", function()
+            if vim.wo.diff then
+               vim.cmd.normal({ "[c", bang = true })
+            else
+               gitsigns.nav_hunk("prev")
+            end
+         end)
+      end,
+   },
 }
