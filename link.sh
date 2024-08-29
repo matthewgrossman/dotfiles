61097a11b89994961e20934bddce39cd425b305e
Matthew Grossman <matthewryangrossman@gmail.com>
2024-08-28 23:41:06 -0700

    fix link.sh again

--- link.sh
+++ link.sh
@@ -4,6 +4,4 @@ set -Eeuxo pipefail
 cd "$HOME/dotfiles/config" && git ls-files \
 	| awk -F'/' '{print $1}' \
 	| sort -u \
-	| xargs -I {} echo "$HOME/dotfiles/config/{}" "$HOME/.config/{}" \
-)
-
+	| xargs -I {} ln -sfn "$HOME/dotfiles/config/{}" "$HOME/.config/{}"
