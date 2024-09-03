5366046c57c8d09b6699e4d1eaaaab8b6465fd96
Matthew Grossman <matthewryangrossman@gmail.com>
2024-09-02 17:16:47 -0700

    add fzf-lua commands

--- config/nvim-revamp/init.lua
+++ config/nvim-revamp/init.lua
@@ -162,7 +162,7 @@ end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
-vim.api.nvim_create_user_command("Z", "w | qa", {})
+vim.api.nvim_create_user_command('Z', 'w | qa', {})
 vim.cmd([[
   cabbrev wqa Z
   cabbrev wqa! Z
@@ -216,9 +216,14 @@ require('lazy').setup({
     -- optional for icon support
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-      -- calling `setup` is optional for customization
       require('fzf-lua').setup({
-        'fzf-vim',
+        "telescope",
+        keymap = {
+          builtin = {
+            ['<C-f>'] = 'preview-down',
+            ['<C-b>'] = 'preview-up',
+          },
+        },
         winopts = {
           preview = {
             layout = 'vertical',
@@ -304,8 +309,6 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
       vim.keymap.set('n', '<C-p>', '<Cmd>Telescope frecency workspace=CWD<CR>')
-      -- vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      -- vim.keymap.set('n', '<leader>sg', '<Cmd>Telescope grep_string search=<CR>', { desc = '[S]earch by [G]rep' })
     end,
   },
 
@@ -457,15 +460,16 @@ require('lazy').setup({
         -- But for many setups, the LSP (`tsserver`) will work just fine
         -- tsserver = {},
         --
-        yamlls = {
-          settings = {
-            yaml = {
-              schemas = {
-                kubernetes = '*',
-              },
-            },
-          },
-        },
+        yamlls = {},
+        -- yamlls = {
+        --   settings = {
+        --     yaml = {
+        --       schemas = {
+        --         kubernetes = '*',
+        --       },
+        --     },
+        --   },
+        -- },
         pyright = {
           settings = {
             pyright = {
