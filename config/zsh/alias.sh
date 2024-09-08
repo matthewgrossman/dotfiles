b345fe5e0fc876acff8d0e09b474154fb1267d9b
Matthew Grossman <matthewryangrossman@gmail.com>
2024-09-08 03:57:35 -0700

    fix aliases

--- config/zsh/alias.sh
+++ config/zsh/alias.sh
@@ -2,7 +2,7 @@ alias ls="ls -AGltr"
 alias dotf='cd $HOME/dotfiles; wezterm cli set-tab-title "dotfiles" 2>/dev/null'
 alias g="git"
 
-alias vi='nvim'
+alias nvim="NVIM_APPNAME=nvim-revamp nvim"
 alias vim='nvim'
 alias k='kubectl'
 alias icat="kitty +kitten icat"
