cbb4a8820a9bb1c033f6bbe79e81839ca56eff35
Matthew Grossman <matthewryangrossman@gmail.com>
2024-09-17 14:48:58 +0200

    more zsh config

--- link.sh
+++ link.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
-set -Eeuxo pipefail
+set -Eeuo pipefail
+
+# Function to create a symbolic link if it doesn't exist
+symlink() {
+    local source="$1"
+    local target="$2"
+    if [ ! -e "$target" ] || [ "$(readlink "$target")" != "$source" ]; then
+        ln -sfn "$source" "$target"
+        echo "Created symlink: $target -> $source"
+    else
+        echo "Symlink already exists: $target -> $source"
+    fi
+}
+export -f symlink
+
+cd "$HOME/dotfiles/config" || exit 1
+
+# zsh doesn't follow XDG standards
+symlink $HOME/dotfiles/config/zsh/.zshenv $HOME/.zshenv
 
 cd "$HOME/dotfiles/config" && git ls-files \
   | awk -F'/' '{print $1}' \
   | sort -u \
-  | xargs -I {} ln -sfn "$HOME/dotfiles/config/{}" "$HOME/.config/{}"
+  | xargs -I {} bash -c 'symlink "$HOME/dotfiles/config/{}" "$HOME/.config/{}"'
