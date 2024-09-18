38301290909ae32669934a1790407b5054c522bc
Matthew Grossman <matthewryangrossman@gmail.com>
2024-09-18 14:36:34 +0200

    add oil and toggleterm

---
+++ config/nvim-revamp/lua/plugins/oil.lua
@@ -0,0 +1,11 @@
+
+return {
+  'stevearc/oil.nvim',
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+  },
+  -- Optional dependencies
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+}
---
+++ config/nvim-revamp/lua/plugins/toggleterm.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    config = function()
+      local percent = 0.95
+      require('toggleterm').setup({
+        float_opts = {
+          width = function()
+            return math.floor(vim.o.columns * percent)
+          end,
+          height = function()
+            return math.floor((vim.o.lines - vim.o.cmdheight) * percent)
+          end,
+        },
+      })
+      vim.keymap.set({ 'n', 't' }, '<C-;>', '<CMD>ToggleTerm direction=float<CR>')
+    end,
+  },
+}
