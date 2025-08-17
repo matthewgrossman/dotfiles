9dfc2596a250c8816d0bfa931bfb40ac36d9cea8
Matthew Grossman <matthewryangrossman@gmail.com>
2025-08-17 16:54:25 -0700

    cleanup setup

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,18 +1,11 @@
-Start-Process powershell -Verb runAs -ArgumentList "-file $PSScriptRoot\setup_admin.ps1"
-
-# no idea of this will work
-[Environment]::SetEnvironmentVariable("XDG_CONFIG_HOME", "\\wsl.localhost\Ubuntu\home\mg\dotfiles\config\wezterm", 'User')
-
 # essentials
 winget install Mozilla.Firefox --accept-package-agreements
 winget install Google.Chrome --accept-package-agreements
-winget install Microsoft.Edge --accept-package-agreements
 winget install Spotify.Spotify --accept-package-agreements
 winget install OpenWhisperSystems.Signal --accept-package-agreements
 winget install Google.Drive --accept-package-agreements
 winget install KeePassXCTeam.KeePassXC --accept-package-agreements
 winget install Microsoft.PowerToys --accept-package-agreements
-winget install SyncTrayzor.SyncTrayzor --accept-package-agreements
 
 # hardware
 winget install Nvidia.GeForceExperience --accept-package-agreements
@@ -21,13 +14,10 @@ winget install AMD.RyzenMaster --accept-package-agreements
 winget install REALiX.HWiNFO --accept-package-agreements
 
 # programming
-winget install wez.wezterm --accept-package-agreements
 winget install Microsoft.WindowsTerminal --accept-package-agreements
-winget install Git.Git --accept-package-agreements
 winget install Microsoft.VisualStudioCode --accept-package-agreements
 winget install RaspberryPiFoundation.RaspberryPiImager --accept-package-agreements
 winget install tailscale.tailscale --accept-package-agreements
-winget install IVPN.IVPN --accept-package-agreements
 winget install RealVNC.VNCViewer --accept-package-agreements
 winget install Iterate.Cyberduck --accept-package-agreements
 winget install calibre.calibre --accept-package-agreements
@@ -35,17 +25,23 @@ winget install calibre.calibre --accept-package-agreements
 # gaming
 winget install Valve.Steam --accept-package-agreements
 winget install EpicGames.EpicGamesLauncher --accept-package-agreements
-winget install Parsec.Parsec --accept-package-agreements
 winget install Discord.Discord --accept-package-agreements
 
+# extras if you want to program on this machine:
+
 # install scoop {{{
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
-irm get.scoop.sh | iex
+# Start-Process powershell -Verb runAs -ArgumentList "-file $PSScriptRoot\setup_admin.ps1"
+#
+# Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
+# irm get.scoop.sh | iex
+#
+# scoop bucket add nerd-fonts
+# scoop bucket add extras
+# scoop update
+#
+# scoop install win32yank
+# scoop install CascadiaCode-NF
+#
+# [Environment]::SetEnvironmentVariable("XDG_CONFIG_HOME", "\\wsl.localhost\Ubuntu\home\mg\dotfiles\config\wezterm", 'User')
 
-scoop bucket add nerd-fonts
-scoop bucket add extras
-scoop update
-
-scoop install win32yank
-scoop install CascadiaCode-NF
 # }}}
