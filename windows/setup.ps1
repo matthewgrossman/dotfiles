427513086ea824da638f3313b80a083d6256f6bf
Matthew Grossman <matthewryangrossman@gmail.com>
2025-08-17 17:03:10 -0700

    more unattended

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,31 +1,31 @@
 # essentials
-winget install Mozilla.Firefox --accept-package-agreements
-winget install Google.Chrome --accept-package-agreements
-winget install Spotify.Spotify --accept-package-agreements
-winget install OpenWhisperSystems.Signal --accept-package-agreements
-winget install Google.Drive --accept-package-agreements
-winget install KeePassXCTeam.KeePassXC --accept-package-agreements
-winget install Microsoft.PowerToys --accept-package-agreements
+winget install Mozilla.Firefox --accept-package-agreements --disable-interactivity --accept-source-agreements
+winget install Google.Chrome --accept-package-agreements --disable-interactivity --accept-source-agreements
+winget install Spotify.Spotify --accept-package-agreements --disable-interactivity --accept-source-agreements
+winget install OpenWhisperSystems.Signal --accept-package-agreements --disable-interactivity --accept-source-agreements
+winget install Google.Drive --accept-package-agreements --disable-interactivity --accept-source-agreements
+winget install KeePassXCTeam.KeePassXC --accept-package-agreements --disable-interactivity --accept-source-agreements
+winget install Microsoft.PowerToys --accept-package-agreements --disable-interactivity --accept-source-agreements
 
 # hardware
-winget install Nvidia.GeForceExperience --accept-package-agreements
-winget install Intel.IntelDriverAndSupportAssistant --accept-package-agreements
-winget install AMD.RyzenMaster --accept-package-agreements
-winget install REALiX.HWiNFO --accept-package-agreements
+winget install Nvidia.GeForceExperience --accept-package-agreements --disable-interactivity --accept-source-agreements
+winget install Intel.IntelDriverAndSupportAssistant --accept-package-agreements --disable-interactivity --accept-source-agreements
+winget install AMD.RyzenMaster --accept-package-agreements --disable-interactivity --accept-source-agreements
+winget install REALiX.HWiNFO --accept-package-agreements --disable-interactivity --accept-source-agreements
 
 # programming
-winget install Microsoft.WindowsTerminal --accept-package-agreements
-winget install Microsoft.VisualStudioCode --accept-package-agreements
-winget install RaspberryPiFoundation.RaspberryPiImager --accept-package-agreements
-winget install tailscale.tailscale --accept-package-agreements
-winget install RealVNC.VNCViewer --accept-package-agreements
-winget install Iterate.Cyberduck --accept-package-agreements
-winget install calibre.calibre --accept-package-agreements
+winget install Microsoft.WindowsTerminal --accept-package-agreements --disable-interactivity --accept-source-agreements
+winget install Microsoft.VisualStudioCode --accept-package-agreements --disable-interactivity --accept-source-agreements
+winget install RaspberryPiFoundation.RaspberryPiImager --accept-package-agreements --disable-interactivity --accept-source-agreements
+winget install tailscale.tailscale --accept-package-agreements --disable-interactivity --accept-source-agreements
+winget install RealVNC.VNCViewer --accept-package-agreements --disable-interactivity --accept-source-agreements
+winget install Iterate.Cyberduck --accept-package-agreements --disable-interactivity --accept-source-agreements
+winget install calibre.calibre --accept-package-agreements --disable-interactivity --accept-source-agreements
 
 # gaming
-winget install Valve.Steam --accept-package-agreements
-winget install EpicGames.EpicGamesLauncher --accept-package-agreements
-winget install Discord.Discord --accept-package-agreements
+winget install Valve.Steam --accept-package-agreements --disable-interactivity --accept-source-agreements
+winget install EpicGames.EpicGamesLauncher --accept-package-agreements --disable-interactivity --accept-source-agreements
+winget install Discord.Discord --accept-package-agreements --disable-interactivity --accept-source-agreements
 
 # extras if you want to program on this machine:
 
